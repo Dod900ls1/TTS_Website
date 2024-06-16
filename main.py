1cb43eef2702f86edabd2d4e4e8a82117eb4d40f
YehorBoiar <boyaregor1@gmail.com>
2024-06-16 10:29:31 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-from speech_synthesis import initialize_device, load_model, split_text, generate_spectrogram, convert_to_audio, concatenate_audio, process_text_to_speech
-import nltk
+from speech_synthesis import initialize_device, load_model, process_text_to_speech
 import nemo.collections.tts as nemo_tts
 import IPython.display as ipd
 from text_processing import pdf_to_text
-
-nltk.download('punkt')
 
 def main():
     device = initialize_device()
     tacotron2 = load_model(nemo_tts.models.Tacotron2Model, "tts_en_tacotron2", device)
     hifigan = load_model(nemo_tts.models.HifiGanModel, "tts_en_hifigan", device)
     
-    large_text = pdf_to_text("sample_test_pdf.pdf")
+    large_text = pdf_to_text("12.pdf")
     
     full_audio = process_text_to_speech(tacotron2, hifigan, device, large_text)
-    output_path = "output_large_text.wav"
+    output_path = "In_Search_Of_The_Miraculous.wav"
     full_audio.export(output_path, format="wav")
     
     return ipd.Audio(output_path)
